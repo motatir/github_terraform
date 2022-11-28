21f9c4638770205fb0b98acc7208c56eba60c29d
Aswini Ray <Aswini.Ray@anz.com>
2022-11-28 13:36:01 +1100

    Provider is downgraded to 5.5.0 due to issue in codefresh pipeline

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 5.9.1"
+      version = "~> 5.5.0"
     }
   }
   required_version = ">= 1.3.2"

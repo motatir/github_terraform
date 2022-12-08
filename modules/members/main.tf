be016f1af7fc9ddb902a1d0add728eccfe5dd81c
motatir <raghavareddy.moatati@anz.com>
2022-12-08 08:46:45 +0530

    moving identify suspended users to main module

--- modules/members/main.tf
+++ modules/members/main.tf
@@ -1,23 +1,10 @@
-data "http" "github_api" {
-  for_each = { for index, lan_id in var.team_members[*] : upper(lan_id) => lan_id }
-  url    = "https://github.service.anz/api/v3/users/${each.value}"
-  # method = "HEAD"
-  
-}
-
-
-
 # adding users to individual teams 
 resource "github_team_membership" "team_membership" {
   for_each = { for index, lan_id in var.team_members[*] : upper(lan_id) => lan_id }
   team_id  = var.team_id
   username = each.value
   role     = contains(var.admins, each.value) ? "maintainer" : "member"
-    lifecycle {
-    precondition {
-      condition = jsondecode(data.http.github_api[each.key].response_body).suspended_at == null
-    }
-  }
+
 }
 
 
--- modules/org_membership/main.tf
+++ modules/org_membership/main.tf
@@ -1,19 +1,7 @@
-data "http" "github_api" {
-  for_each = { for index, lan_id in var.org_members[*] : upper(lan_id) => lan_id }
-  url    = "https://github.service.anz/api/v3/users/${each.value}"
-  # method = "HEAD"
-  
-}
-
 
 resource "github_membership" "membership_for_org" {
   for_each = { for index, lan_id in var.org_members[*] : upper(lan_id) => lan_id }
   username = each.value
 
-  lifecycle {
-    precondition {
-      condition = jsondecode(data.http.github_api[each.key].response_body).suspended_at == null
-      error_message = "${each.value} is suspended user"
-    }
-  }
+
 }
